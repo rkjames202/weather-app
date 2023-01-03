bf6c1201201e76e4ca2af1f189adf5e8b685ab18
Russell James <russelljames202@gmail.com>
2023-01-03 18:04:06 -0500

    Make default query and add listeners

--- src/index.js
+++ src/index.js
@@ -2,17 +2,9 @@ import createQueryCall from "./fetchData";
 import displayData from "./displayData";
 import addListeners from "./addListeners";
 
-/**
- * Commit
- * Complete layout / style
- * Commit
- * Add Icons
- */
-
-function main(){
- createQueryCall("Baltimore").then(displayData);
- addListeners();
+function main() {
+  createQueryCall("Cambridge").then(displayData);
+  addListeners();
 }
 
 main();
-
