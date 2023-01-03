d1180c72c5237f88d0106ada182a2cec9d549984
Russell James <russelljames202@gmail.com>
2023-01-03 17:31:34 -0500

    Add weather icon to display and refactor
    Added displayWeatherIcon() which displays a weather icon depending on
    the current weather condition.
    
    DisplayErrorMsg() and hideErrorMsg() now change the visibility of the
    error message instead of its inner text.
    
    DisplayData() now changes the visibility of the div with a class of
    “main” and the toggle units button.
    
    displayTemps() and displayWinds() are now exported for later use in
    addListeners.js

--- src/displayData.js
+++ src/displayData.js
@@ -16,7 +16,7 @@ function displayWeatherdesc(desc) {
   descPara.innerText = newDesc;
 }
 function displayDate(dates) {
-  const formattedDate = format(dates.current, "EEEE, MMM do y");
+  const formattedDate = format(dates.current, "EEEE, MMM do ''yy");
   const datePara = document.querySelector(".date");
   datePara.innerText = formattedDate;
 }
@@ -58,7 +58,7 @@ function displayTemps(temps, option) {
   const feelsLikeTempPara = document.querySelector(".feels-like-temp");
   feelsLikeTempPara.innerText = `Feels Like: ${
     temps.feelsLike[scale]
-  }°${option.toUpperCase()}`
+  }°${option.toUpperCase()}`;
 
   const highTempPara = document.querySelector(".high-temp");
   highTempPara.innerText = `High: ${
@@ -81,24 +81,64 @@ function displayTimes(dates) {
   sunsetTime.innerText = `Sunset: ${format(dates.sunset, "p")}`;
 }
 
-function displayErrorMsg() {
-    const errorMsg = document.querySelector(".error-msg");
-    const input = document.querySelector("input");
-  
-    errorMsg.innerText =
-      "Query must be in the form of... \n <City>, \n <City, State>, \n or <City, Country>";
-      input.value = "";
-  }
-  
-  function hideErrorMsg() {
-    const errorMsg = document.querySelector(".error-msg");
-    const input = document.querySelector("input");
-  
-    errorMsg.innerText = "";
-    input.value = "";
+function displayWeatherIcon(responseData) {
+  const weatherDesc = responseData.desc;
+  const currentHours = new Date(responseData.dates.current).getHours();
+  const sunsetHours = new Date(responseData.dates.sunset).getHours();
+  const sunriseHours = new Date(responseData.dates.sunrise).getHours();
+
+  let timeofDay;
+  if (currentHours <= sunsetHours && currentHours >= sunriseHours) {
+    timeofDay = "day";
+  } else {
+    timeofDay = "night";
   }
 
+  const icon = document.querySelector(".weather-icon-wrapper i");
+  if (weatherDesc.includes("thunderstorm")) {
+    icon.className = "fa-solid fa-cloud-bolt";
+  } else if (weatherDesc.includes("rain")) {
+    icon.className = "fa-solid fa-cloud-rain";
+  } else if (weatherDesc.includes("snow")) {
+    icon.className = "fa-regular fa-snowflake";
+  } else if (weatherDesc.includes("clouds") && timeofDay === "day") {
+    icon.className = "fa-solid fa-cloud-sun";
+  } else if (weatherDesc.includes("clouds") && timeofDay === "night") {
+    icon.className = "fa-solid fa-cloud-moon";
+  } else if (weatherDesc.includes("clear") && timeofDay === "day") {
+    icon.className = "fa-solid fa-sun";
+  } else if (weatherDesc.includes("clear") && timeofDay === "night") {
+    icon.className = "fa-solid fa-moon";
+  } else if (weatherDesc.includes("mist") || weatherDesc.includes("fog")) {
+    icon.className = "fa-solid fa-smog";
+  } else if (weatherDesc.includes("tornado")) {
+    icon.className = "fa-solid fa-tornado";
+  }
+}
+
+function displayErrorMsg() {
+  const errorMsg = document.querySelector(".error-msg");
+  const input = document.querySelector("input");
+
+  errorMsg.style.visibility = "visible";
+  input.value = "";
+}
+
+function hideErrorMsg() {
+  const errorMsg = document.querySelector(".error-msg");
+  const input = document.querySelector("input");
+
+  errorMsg.style.visibility = "hidden";
+  input.value = "";
+}
+
 function displayData(responseData) {
+  const main = document.querySelector(".main");
+  const toggleButton = document.querySelector(".toggle-units");
+
+  main.style.visibility = "visible";
+  toggleButton.style.visibility = "visible";
+
   displayLocation(responseData.location);
   displayWeatherdesc(responseData.desc);
   displayDate(responseData.dates);
@@ -106,9 +146,10 @@ function displayData(responseData) {
   displayTemps(responseData.temps, "f");
   displayHumidity(responseData.humidity);
   displayWinds(responseData.wind, "mph");
+  displayWeatherIcon(responseData);
 
   hideErrorMsg();
 }
 
 export default displayData;
-export { displayErrorMsg };
+export { displayErrorMsg, displayTemps, displayWinds };
