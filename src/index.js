a087968f7a903d75d7ad8a13efeaa09236a899ac
Russell James <russelljames202@gmail.com>
2023-01-03 20:45:30 -0500

    Add comments and new function, addTabIcon()
    addTabIcon() creates a link element to display the tab icon.

--- src/index.js
+++ src/index.js
@@ -1,8 +1,26 @@
 import createQueryCall from "./fetchData";
 import displayData from "./displayData";
 import addListeners from "./addListeners";
+import tabIcon from "./sun.png"
 
+/**
+ * Add tab icon
+ */
+function addTabIcon(){
+    const link = document.createElement("link");
+    const head = document.querySelector("head");
+
+    link.rel = "icon";
+    link.href = tabIcon;
+
+    head.appendChild(link);
+}
+
+/**
+ * Call necessary functions along with default query
+ */
 function main() {
+  addTabIcon();
   createQueryCall("Cambridge").then(displayData);
   addListeners();
 }
